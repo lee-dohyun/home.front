b91cfe7dc7d7a375911384477fa0a4e5548f4c0f
lee-dohyun <leedohyun1985@gmail.com>
2025-06-25 10:52:56 +0900

    설정 변경 테스트

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  reactStrictMode: true,
+  poweredByHeader: false,
+  swcMinify: true,
+  // 외부에서 호출될 경우, 애플리케이션이 위치한 경로 기준을 명확히 설정
+  basePath: "",
+  assetPrefix: "/",
+  // 이미지 도메인이 있다면 (예: CDN)
+  images: {
+    domains: ["leedohyun.com"],
+  },
 };
 
 export default nextConfig;
